daa8e13a386334415da057bb9422a07b5180ad8e
Lindan Thillanayagam <lindx4@gmail.com>
2018-12-27 12:15:20 -0500

    Learned how to create a component and learned how to add dynamic properties.

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi. I am some react app.</h1>
+        <p>I don't know why though.</p>
+        <Person />
+        <Person />
+        <Person />
       </div>
+
     );
+    //return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hello. I am a basic React app.'));
   }
 }
 
